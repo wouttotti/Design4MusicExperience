0b2ffd8e74b5f3a0fd397003caefa93c5b9e61b3
Qunfong <qunfong@live.nl>
2017-09-26 13:44:59 +0200

    Added startscreen

---
+++ Assets/Myo Samples/Scripts/buttonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonManager : MonoBehaviour {
+
+    public void activateIntensity()
+    {
+
+    }
+    public void activateColorPalette()
+    {
+
+    }
+    public void activateFlash()
+    {
+
+    }
+    public void activateStrobocope()
+    {
+
+    }
+    public void activateKnightrider()
+    {
+
+    }
+}
